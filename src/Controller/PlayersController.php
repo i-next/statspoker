91d658ac4e0a3abcdc7a713625f5b177d2cc62bf
Emmanuel euchin <euchin@i-next.fr>
2022-11-24 17:15:20 +0100

    feat bootstraptable

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\ElasticaBundle\Index\IndexManager;
@@ -29,22 +30,35 @@ class PlayersController extends AbstractController
 
 
     #[Route('/ajaxplayers', name: 'app_ajax_players')]
-    public function ajaxPlayers(): JsonResponse
+    public function ajaxPlayers(Request $request): JsonResponse
     {
         $players = [];
         $queryPlayers = new Query();
-        $queryPlayers->setSize(50000);
+        $size = $request->query->get('limit')?:50000;
+        $order = $request->query->get('order')?:'asc';
+        $sort = $request->query->get('sort')?:'pseudo';
+        $offset = $request->query->get('offset')?:0;
+        $search = $request->query->get('search')?:false;
+        $queryPlayers->setFrom($offset);
+        $queryPlayers->setSize($size);
+        $queryPlayers->setSort([$sort=>$order]);
+        if($search){
+            $queryString = new Query\QueryString();
+            $queryString->setDefaultField('pseudo');
+            $queryString->setQuery('*'.$search.'*');
+            $queryPlayers->setQuery($queryString);
+        }
         $playersES = $this->indexManager->getIndex('joueurs')->search($queryPlayers)->getResults();
         foreach($playersES as $key =>$playerES){
 
             $playerData = $playerES->getData();
             $players[$key]['pseudo'] = $playerData['pseudo'];
-            $players[$key]['hand'] = $playerData['hand_win'];
-            $players[$key]['tour'] = $playerData['tour_win'];
+            $players[$key]['hand_win'] = $playerData['hand_win'];
+            $players[$key]['tour_win'] = $playerData['tour_win'];
         }
         return new JsonResponse([
             'rows' => $players,
-            'total'=> count($playersES)
+            'total'=> $this->indexManager->getIndex('joueurs')->count()
         ]);
     }
 }
